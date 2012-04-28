9f41a0443813ebb78e89357c998a0e69025f5dea
Emmanuel Nhan <nhanmanu@gmail.com>
2012-04-28 23:22:23 +0200

    added a basic test and support for AspectJ in pom

---
+++ src/test/java/eu/enhan/socialist/test/model/UserTest.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package eu.enhan.socialist.test.model;
+
+import org.junit.Test;
+
+import eu.enhan.socialist.model.User;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Emmanuel Nhan
+ *
+ */
+public class UserTest {
+	
+	@Test
+	public void testEqualsDefault(){
+		
+		User u = new User();
+		User u2 = new User();
+		User u3 = new User("plop");
+		
+		assertTrue(u.equals(u2));
+		assertTrue(u2.equals(u));
+		assertFalse(u3.equals(u));
+		
+	}
+
+}
