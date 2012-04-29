986bd4f35e8b72b3412d6278c6dff860a4f95a3f
Emmanuel Nhan <nhanmanu@gmail.com>
2012-04-29 22:55:11 +0200

    added Bootstrap from Twitter resources and resource serving

--- src/main/java/eu/enhan/socialist/web/config/WebConfig.java
+++ src/main/java/eu/enhan/socialist/web/config/WebConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
@@ -16,7 +18,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="eu.enhan.socialist.web")
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter{
 	
 	@Bean
 	public InternalResourceViewResolver resourceResolver(){
@@ -25,6 +27,15 @@ public class WebConfig {
 		resolver.setSuffix(".jsp");
 		return resolver;
 	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		
+		
+	}
+	
+	
 
 
 }
--- src/main/webapp/WEB-INF/jsp/hello.jsp
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -1,13 +1,15 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html>
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Timelord</title>
-</head>
-<body>
-	<h1>Hello from Socialist</h1>
-</body>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="/socialist/resources/css/bootstrap.css">
+        <title>Socialist</title>
+    </head>
+
+    <body>
+    	<h1>Hello from Socialist</h1>
+    </body>
 </html>
